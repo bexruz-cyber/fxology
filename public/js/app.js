b8d52fcbe8a5e210b35f33dd3f2126074dfd7658
bexruz-cyber <mayoq2024@gmail.com>
2024-09-20 10:02:40 +0500

    add

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,27 @@ inp_box.forEach(function(item, index) {
     item.addEventListener("click", function() {
         item.classList.toggle("py-[22px]");
         item.classList.toggle("py-[12px]");
-        input_ui[index].classList.toggle("h-0")
-        input_ui[index].classList.toggle("h-2.5")
-        input_ui[index].classList.toggle("mt-3")
+        input_ui[index].classList.toggle("h-0");
+        input_ui[index].classList.toggle("h-2.5");
+        input_ui[index].classList.toggle("mt-3");
+        
     });
 });
+
+const api = "https://restcountries.com/v3.1/all";
+
+// API'dan ma'lumot olish
+fetch(api)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json(); // JSON formatda ma'lumotlarni qaytaradi
+    })
+    .then(data => {
+        console.log(data); // Barcha davlatlar haqidagi ma'lumotlarni konsolga chiqaradi
+        // Ma'lumotlarni qayta ishlash yoki UI'ga qo'shish uchun kod yozish mumkin.
+    })
+    .catch(error => {
+        console.error('Error:', error); // Xatolik bo'lsa, uni konsolga chiqaradi
+    });
